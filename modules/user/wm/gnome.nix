1fe9c73b40fd457b84510df26c4ac9358b47cacc
Richard Tanner <rtanner@myron.com>
2025-09-13 14:25:44 -0400

    Gnome set dark mode

--- modules/user/wm/gnome.nix
+++ modules/user/wm/gnome.nix
@@ -20,5 +20,11 @@
       name = "Bibata-Modern-Ice";
       size = 22;
     };
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
   };
 }
